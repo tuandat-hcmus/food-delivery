8bbf89369b017b075d1e7e88234f1db5f557406b
21120430 <ngotuandat.lop9a@gmail.com>
2025-03-04 00:31:04 +0700

    standardlize error response

---
+++ common/app_error.go
@@ -0,0 +1,66 @@
+package common
+
+import (
+	"errors"
+	"net/http"
+)
+
+type AppError struct {
+	StatusCode int    `json:"status_code"`
+	RootErr    error  `json:"_"`
+	Message    string `json:"message"`
+	Log        string `json:"log"`
+	Key        string `json:"error_key"`
+}
+
+func NewErrorResponse(root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusBadRequest,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewFullErrorResponse(statusCode int, root error, msg, log, key string) *AppError {
+	return &AppError{
+		StatusCode: statusCode,
+		RootErr:    root,
+		Message:    msg,
+		Log:        log,
+		Key:        key,
+	}
+}
+
+func NewUnauthorized(root error, msg, key string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusUnauthorized,
+		RootErr:    root,
+		Message:    msg,
+		Key:        key,
+	}
+}
+
+func NewCustomError(root error, msg, key string) *AppError {
+	if root != nil {
+		return NewErrorResponse(root, msg, root.Error(), key)
+	}
+	return NewErrorResponse(errors.New(msg), msg, msg, key)
+}
+
+func (e *AppError) RootError() error {
+	if err, ok := e.RootErr.(*AppError); ok {
+		return err.RootError()
+	}
+	return e.RootErr
+}
+
+func (e *AppError) Error() string {
+	return e.RootError().Error()
+}
+
+// db errors
+func ErrDB(err error) *AppError {
+	return NewErrorResponse(err, "something went wrong with database", err.Error(), "DB_ERROR")
+}
