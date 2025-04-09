e3d23d7fd9e7dc37a5d990ad6a964e10d26c6456
21120430 <ngotuandat.lop9a@gmail.com>
2025-04-09 19:02:26 +0700

    refactor: add jwt provider

--- component/tokenprovider/jwt/jwt.go
+++ component/tokenprovider/jwt/jwt.go
@@ -1,2 +1,42 @@
 package jwt
 
+import (
+	"rest/component/tokenprovider"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type jwtProvider struct {
+	secret string
+}
+
+func NewTokenJwtProvider(secret string) *jwtProvider {
+	return &jwtProvider{secret: secret}
+}
+
+type myClaims struct {
+	PayLoad tokenprovider.TokenPayLoad `json:"payload"`
+	jwt.RegisteredClaims
+}
+
+func (j *jwtProvider) Generate(data tokenprovider.TokenPayLoad, expiry int) (*tokenprovider.Token, error) {
+	// generate jwt
+	t := jwt.NewWithClaims(jwt.SigningMethodES256, myClaims{
+		data,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().UTC().Add(time.Second * time.Duration(expiry))),
+			IssuedAt: jwt.NewNumericDate(time.Now().UTC()),
+		},
+	})
+	myToken, err := t.SignedString(j.secret)
+	if err != nil {
+		return nil, err
+	}
+	// return token 
+	return &tokenprovider.Token{
+		Token: myToken,
+		Expiry: expiry,
+		Created: time.Now().UTC(),
+	}, nil
+}
